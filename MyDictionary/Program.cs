b422423a4144096228a045e8da0c11b61d29baba
seliudev <ulucamselinn@gmail.com>
2024-08-12 10:46:49 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿//
+//C# Dictionary practice using .Add();
+
+Dictionary<int,string> stock = new Dictionary<int,string>();
+stock.Add(101 ,"Pencil");
+stock.Add(102,"Eraser");
+stock.Add(103,"Notebook");
+stock.Add(104,"Scissor");
+stock.Add(105,"Crayon");
+stock.Add(106, "Marker");
+stock.Add(107, "Highlighter");
+
+foreach (var product in stock)
+{
+    Console.WriteLine("Product name: " + product.Value + " |" + " Product code: " + product.Key);
+}
+
+
+
+
